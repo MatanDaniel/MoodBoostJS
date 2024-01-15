de6aa91388144c22fcd7f82e643ccd53c2ec32e0
ImTheRisingPhoenix <sak7520@gmail.com>
2024-01-15 21:51:26 +0200

    basic design update
    updated the design and make of the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div>
+      <p className='Title'>MoodBoost: Your daily boost of music</p>
+      <p className='Explain'>Hiya, and welcome to MoodBoost!<br />
+        Moodboost is a music player that wants to boost your morale and help to improve your mood throughout the day!<br />
+        Both designers, Rahamim Ram Eliyaou and Matan Daniel have made this application in order to spread good emotion or good "vibes"<br />
+        to help people in these trying times, and what better to effect a person's mood with music?<br />
+        Feel free to sign up to the app or log in if you already have an account and we can get your MoodBoost started!
+      </p>
+    </div><div className='buttons'>
+        <Button variant='info'>Sign up</Button> <Button variant='primary'>Log in</Button>
+      </div></>
   );
 }
 
