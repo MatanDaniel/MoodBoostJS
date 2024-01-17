e474d2d24dffbb0f1a72c88b909b9215b4e153c2
ImTheRisingPhoenix <sak7520@gmail.com>
2024-01-17 15:26:55 +0200

    got material UI to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css';
-import {Button} from 'react-bootstrap'
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <><div>
-      <p className='Title'>MoodBoost: Your daily boost of music</p>
-      <p className='Explain'>Hiya, and welcome to MoodBoost!<br />
+    <>
+      <div>
+        <p className='Title'>MoodBoost: Your daily boost of music</p>
+        <p className='Explain'>Hiya, and welcome to MoodBoost!<br />
         Moodboost is a music player that wants to boost your morale and help to improve your mood throughout the day!<br />
         Both designers, Rahamim Ram Eliyaou and Matan Daniel have made this application in order to spread good emotion or good "vibes"<br />
         to help people in these trying times, and what better to effect a person's mood with music?<br />
         Feel free to sign up to the app or log in if you already have an account and we can get your MoodBoost started!
-      </p>
-    </div><div className='buttons'>
-        <Button variant='info'>Sign up</Button> <Button variant='primary'>Log in</Button>
-      </div></>
+        </p>
+      </div>
+      <div className='buttons'>
+          <Button variant='contained'>Login</Button> <Button variant='contained' color='secondary'>Sign up</Button>
+      </div>
+    </>
   );
 }
 
